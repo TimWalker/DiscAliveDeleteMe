3a27abccb66e92520375ce59c8e8b064f60ee03d
Tim Walker <walketim@gmail.com>
2016-10-20 17:29:07 -0500

    fix cucumber jvm dependency mismatch by adding a section with matching verion in build.gradle

--- app/report/report.js
+++ app/report/report.js
@@ -28,7 +28,7 @@ formatter.match({
   "location": "rfid_steps.there_are_no_RFIDs_in_Proximity()"
 });
 formatter.result({
-  "duration": 241903139,
+  "duration": 118903316,
   "error_message": "cucumber.api.PendingException: TODO: implement me\n\tat com.example.theagiletester.discalive.step_definitions.rfid_steps.there_are_no_RFIDs_in_Proximity(rfid_steps.java:17)\n\tat ✽.Given there are no RFIDs in Proximity(DiscAlive.feature:3)\n",
   "status": "pending"
 });
@@ -66,7 +66,7 @@ formatter.match({
   "location": "rfid_steps.i_am_in_proximity_of_an_RFID_Tag()"
 });
 formatter.result({
-  "duration": 278670,
+  "duration": 682453,
   "error_message": "cucumber.api.PendingException: TODO: implement me\n\tat com.example.theagiletester.discalive.step_definitions.rfid_steps.i_am_in_proximity_of_an_RFID_Tag(rfid_steps.java:29)\n\tat ✽.Given I am in proximity of an RFID Tag(DiscAlive.feature:7)\n",
   "status": "pending"
 });
@@ -98,7 +98,7 @@ formatter.match({
   "location": "rfid_steps.i_can_read_an_RFID_Tag()"
 });
 formatter.result({
-  "duration": 198472,
+  "duration": 207630,
   "error_message": "cucumber.api.PendingException: TODO: implement me\n\tat com.example.theagiletester.discalive.step_definitions.rfid_steps.i_can_read_an_RFID_Tag(rfid_steps.java:41)\n\tat ✽.Given I can read an RFID Tag(DiscAlive.feature:11)\n",
   "status": "pending"
 });
@@ -130,7 +130,7 @@ formatter.match({
   "location": "rfid_steps.i_my_Android_phone_is_in_proximity_of_an_NFC_RFID()"
 });
 formatter.result({
-  "duration": 218011,
+  "duration": 208973,
   "error_message": "cucumber.api.PendingException: TODO: implement me\n\tat com.example.theagiletester.discalive.step_definitions.rfid_steps.i_my_Android_phone_is_in_proximity_of_an_NFC_RFID(rfid_steps.java:53)\n\tat ✽.Given I my Android phone is in proximity of an NFC RFID(DiscAlive.feature:15)\n",
   "status": "pending"
 });
---
+++ app/src/main/java/com/example/theagiletester/discalive/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.theagiletester.discalive;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+//A fully functional app at https://github.com/osmas/AndroidNFCReadApp
+
+/**
+ * Activity for reading data from an NDEF Tag.
+ *
+ * @author Ralf Wondratschek
+ */
+public class MainActivity extends Activity {
+
+    public static final String TAG = "NfcDemo";
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("On Create");
+        setContentView(R.layout.activity_main);
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+//        return true;
+//    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mTextView = (TextView) findViewById(R.id.textView_explanation);
+//
+//        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+//
+//        if (mNfcAdapter == null) {
+//            // Stop here, we definitely need NFC
+//            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+//            finish();
+//            return;
+//
+//        }
+//
+//        if (!mNfcAdapter.isEnabled()) {
+//            mTextView.setText("NFC is disabled.");
+//        } else {
+//            mTextView.setText(R.string.explanation);
+//        }
+//
+//        handleIntent(getIntent());
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+//    }
+//
+//    private void handleIntent(Intent intent) {
+//        // TODO: handle Intent
+//    }
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        client.connect();
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "Main Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app URL is correct.
+//                Uri.parse("android-app://com.example.theagiletester.discalive/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.start(client, viewAction);
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "Main Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app URL is correct.
+//                Uri.parse("android-app://com.example.theagiletester.discalive/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.end(client, viewAction);
+//        client.disconnect();
+//    }
+
+    private class GoogleApiClient {
+    }
+}
